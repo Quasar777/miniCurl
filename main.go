887447fbb64731d886d2ed438341876bc2284fbc
sarmat <tsaloev.sarmat@gmail.com>
2025-08-07 11:50:28 +0300

    parsing several headers implementation

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"flag"
+	"fmt"
+	"strings"
 )
 
 
 
 func main() {
-	fmt.Println("args: ", os.Args[1:])
-	method := flag.String("X", "GET", "HTTP method (GET, POST, etc.)")
+	method := flag.String("X", "GET", "HTTP method")
+	headers := flag.String("H", "", "Headers of request")
+	body := flag.String("d", "", "Body of request")
+	fileNameForSaving := flag.String("o", "", "file name for saving")
 	flag.Parse()
-	fmt.Println(*method)	
+
+	fmt.Println("method: ", *method)	
+	GetHeaders(*headers)
+	fmt.Println("body: ", *body)	
+	fmt.Println("file for saving: ", *fileNameForSaving)	
+
+	GetHeaders(*headers)
+}
+
+func GetHeaders(rawStr string) []string {
+	headers := strings.Split(rawStr, ";")
+
+	/*
+	for i := range headers {
+		headers[i] = strings.Trim(headers[i], " ")
+		fmt.Println(headers[i])
+	}
+	*/
+
+	return headers
 }
